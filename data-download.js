075c1d3d60b7e59e7446c3179a039cca8fba70a8
fornaeffe <fornaeffe@gmail.com>
2024-08-10 19:59:36 +0200

    data download script

---
+++ data-download.js
@@ -0,0 +1,36 @@
+// From https://stackoverflow.com/questions/37614649/how-can-i-download-and-save-a-file-using-the-fetch-api-node-js
+
+import { resolve } from 'path';
+import { existsSync, createWriteStream, unlink } from 'fs';
+import { mkdir } from 'fs/promises';
+import { Readable } from 'stream';
+import { finished } from 'stream/promises';
+
+
+console.log("Starting data download...")
+
+if (!existsSync("public/data")) await mkdir("public/data");
+
+const downloadFile = (async (url, fileName) => {
+    const destination = resolve("./public/data", fileName);
+    const fileStream = createWriteStream(destination);
+    try {
+        const res = await fetch(url);
+        
+        if (!res.ok) {
+            throw new Error(`Download error: ${response.statusText}`);
+        }
+
+        await finished(Readable.fromWeb(res.body).pipe(fileStream));
+        
+        console.log("Downloaded " + fileName + " from " + url)
+    } catch (err) {
+        console.error('Error downloading file ' + fileName + ':', err.message);
+        unlink(destination, (err) => {
+            if (err) console.error('Error in removing partially downloaded file ' + fileName + ':', err.message);
+        });
+    }
+     
+})
+
+await downloadFile("https://elezioni.interno.gov.it/daithome/documenti/Europee_Scrutini_ITALIA_20240609.csv", "europee_2024.csv")
